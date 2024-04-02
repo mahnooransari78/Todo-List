043a300421f3209cabb174eaf28905138ef62828
mahnooransari78 <164240218+mahnooransari78@users.noreply.github.com>
2024-04-02 13:48:28 +0500

    inital commit

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//Arrey:
+let todolist = [];
+let condition = true;
+//loop:
+while (condition) {
+    const answer = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What you went to add in your todo?"
+        },
+        {
+            name: "moretodo",
+            type: "confirm",
+            message: "Do you went add more in your todo?",
+            default: "fasle",
+        }
+    ]);
+    //       Arrey:
+    todolist.push(answer.todo);
+    //       loop:
+    condition = answer.moretodo;
+    //       console arrey: 
+    console.log(todolist);
+}
